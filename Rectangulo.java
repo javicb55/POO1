668948a07fa6c612fb9feb1372e6074f96361f01
Pepe <[javicb55@gmail.com]>
2016-12-02 10:12:08 +0100

    Practicas

---
+++ Rectangulo.java
@@ -0,0 +1,6 @@
+public class Rectangulo
+{
+	//atributos
+	private int lado1 = 2;
+	private int lado2 = 1;
+}
---
+++ TestRectangulo.java
@@ -0,0 +1,11 @@
+public class TestRectangulo
+{
+	public static void main (String[] arg)
+	{
+		//Creamos objetos de tipo Triangulo
+		Rectangulo r1 = new Rectangulo();
+		Rectangulo r2 = new Rectangulo();
+		System.out.println(r1.lado1);
+		System.out.println(r2.lado2);
+	}
+}
