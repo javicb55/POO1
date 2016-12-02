3fadaad05d0e3998035b4c002647f27ae5a9f881
Pepe <[javicb55@gmail.com]>
2016-12-02 11:45:46 +0100

    Trabajando POO

--- Rectangulo.java
+++ Rectangulo.java
@@ -3,4 +3,30 @@ public class Rectangulo
 	//atributos
 	private int lado1 = 2;
 	private int lado2 = 1;
+	//getters son publicos
+	public int getLado1()
+	{
+		return this.lado1;
+	}
+	public int getLado2()
+	{
+		return this.lado2;
+	}
+	//setters
+	public void setLado1(int lado1) 
+	{
+		this.lado1 = lado1;
+	}
+	public void setLado2(int lado2)
+	{
+		this.lado2 = lado2;
+	}
+	//método que devuelve el valor del area
+	public int getArea()
+	{
+		return  this.lado1*this.lado2;
+	}
+	
+
 }
+
--- TestRectangulo.java
+++ TestRectangulo.java
@@ -5,7 +5,12 @@ public class TestRectangulo
 		//Creamos objetos de tipo Triangulo
 		Rectangulo r1 = new Rectangulo();
 		Rectangulo r2 = new Rectangulo();
-		System.out.println(r1.lado1);
-		System.out.println(r2.lado2);
+	//	System.out.println(r1.lado1); NO tiene accesibilidad
+	//	System.out.println(r2.lado2); lado1 y lado2 son atributos private
+	//cambiamos los valores por defecto del rectangulo r2
+		r2.setLado1(100); //actualizo los valores del atribulo lado1
+		r2.setLado2(1000);//actualizo los valores del atributo lado2
+		System.out.printf("Atributos del Rectángulo1, lado1: %d, lado2:%d%n y de área %d%n", r2.getLado1(), r2.getLado2(), r2.getArea());
+		System.out.printf("Atributos del Rectángulo1, lado1: %d, lado2:%d%n  de área %d%n ", r1.getLado1(), r1.getLado2(), r1.getArea());
 	}
 }
